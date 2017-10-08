3b420857d8e6cc672d2906e93f2497d06f61f946
Luciany Laurentino <lucylaurentino@gmail.com>
2017-10-08 13:07:43 +1300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function tempConvert(type) {
+    var docF = document.getElementById("far");
+    var docC = document.getElementById("cel");
+    var temp;
+
+}
+let Calculate = (input1, input2, degrees) => {
+    let answer = "";
+    switch (degree) {
+        case(""):
+        answer = 
+
+    }
+}
+
+switch (type) {
+    case 'F':
+    tempF = Number(docF.value);
+    docF.style.backgroundColor = '#fff'
+    tempC = (tempF - 32) * 1.8;
+    break;
+    case 'C':
+    tempC = Number(docC.value);
+    docC.style.backgroundColor = '#fff'
+    tempF = (tempC * 1.8) + 32;
+    break;
+    default:
+    
+
+}
+
+docF.value = tempF;
+docC.value = tempC;
